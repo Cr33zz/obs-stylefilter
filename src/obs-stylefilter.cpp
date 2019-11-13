cfb0b12e82766c50e111859076f7268d48f7bc94
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-13 12:56:23 -0500

    Integrating fnst.

--- src/obs-stylefilter.cpp
+++ src/obs-stylefilter.cpp
@@ -17,6 +17,74 @@
 
 using namespace Neuro;
 
+static ModelBase* generator = nullptr;
+static Placeholder* input = nullptr;
+static TensorLike* stylizedContentPre = nullptr;
+
+class OutputScale : public LayerBase
+{
+public:
+    OutputScale(const string& name = "") : LayerBase(__FUNCTION__, Shape(), name) {}
+protected:
+    virtual vector<TensorLike*> InternalCall(const vector<TensorLike*>& inputNodes, TensorLike* training) override { return { multiply(inputNodes[0], 150.f) }; }
+};
+
+static ModelBase* create_generator_model(TensorLike* input)
+{
+    NameScope scope("generator");
+
+    auto training = new Constant(0);
+
+    auto residual_block = [&](TensorLike* x, int num)
+    {
+        auto shortcut = x;
+        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_1"))->Call(x)[0];
+        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_1"))->Call(x, training)[0];
+        x = (new Activation(new ReLU(), "resi_relu_" + to_string(num) + "_1"))->Call(x)[0];
+        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_2"))->Call(x)[0];
+        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_2"))->Call(x, training)[0];
+        auto m = (new Merge(SumMerge, nullptr, "resi_add_" + to_string(num)))->Call({ x, shortcut })[0];
+        return m;
+    };
+
+    auto input_o = new Input(input, "input_o");
+
+    auto c1 = (new Conv2D(32, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_1"))->Call(input_o->Outputs())[0];
+    c1 = (new InstanceNormalization("normal_1"))->Call(c1, training)[0];
+    c1 = (new Activation(new ReLU(), "relu_1"))->Call(c1)[0];
+
+    auto c2 = (new Conv2D(64, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_2"))->Call(c1)[0];
+    c2 = (new InstanceNormalization("normal_2"))->Call(c2, training)[0];
+    c2 = (new Activation(new ReLU(), "relu_2"))->Call(c2)[0];
+
+    auto c3 = (new Conv2D(128, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_3"))->Call(c2)[0];
+    c3 = (new InstanceNormalization("normal_3"))->Call(c3, training)[0];
+    c3 = (new Activation(new ReLU(), "relu_3"))->Call(c3)[0];
+
+    auto r1 = residual_block(c3, 1);
+    auto r2 = residual_block(r1, 2);
+    auto r3 = residual_block(r2, 3);
+    auto r4 = residual_block(r3, 4);
+    auto r5 = residual_block(r4, 5);
+
+    auto d1 = (new UpSampling2D(2, "up_1"))->Call(r5)[0];
+    d1 = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_4"))->Call(d1)[0];
+    d1 = (new InstanceNormalization("normal_4"))->Call(d1, training)[0];
+    d1 = (new Activation(new ReLU(), "relu_4"))->Call(d1)[0];
+
+    auto d2 = (new UpSampling2D(2, "up_2"))->Call(d1)[0];
+    d2 = (new Conv2D(32, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_5"))->Call(d2)[0];
+    d2 = (new InstanceNormalization("normal_5"))->Call(d2, training)[0];
+    d2 = (new Activation(new ReLU(), "relu_5"))->Call(d2)[0];
+
+    auto c4 = (new Conv2D(3, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_6"))->Call(d2)[0];
+    c4 = (new InstanceNormalization("normal_6"))->Call(c4, training)[0];
+    c4 = (new Activation(new Tanh(), "tanh_1"))->Call(c4)[0];
+    c4 = (new OutputScale("output"))->Call(c4)[0];
+
+    return new Flow(input_o->Outputs(), { c4 }, "generator_model");
+}
+
 static void RGB_2_I420(const uint8_t* rgb, obs_source_frame* frame)
 {
     uint32_t image_size = frame->width * frame->height;
@@ -30,15 +98,17 @@ static void RGB_2_I420(const uint8_t* rgb, obs_source_frame* frame)
         {
             for (size_t x = 0; x < frame->width; x += 2)
             {
-                uint8_t r = rgb[3 * i], g = rgb[3 * i + 1], b = rgb[3 * i + 2];
+                uint8_t r = rgb[i], g = rgb[i + 1], b = rgb[i + 2];
+                rgb += 3;
 
                 frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
                 frame->data[1][upos++] = ((-38 * r + -74 * g + 112 * b) >> 8) + 128;
                 frame->data[2][vpos++] = ((112 * r + -94 * g + -18 * b) >> 8) + 128;
 
-                r = rgb[3 * i];
-                g = rgb[3 * i + 1];
-                b = rgb[3 * i + 2];
+                r = rgb[i];
+                g = rgb[i + 1];
+                b = rgb[i + 2];
+                rgb += 3;
 
                 frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
             }
@@ -47,7 +117,47 @@ static void RGB_2_I420(const uint8_t* rgb, obs_source_frame* frame)
         {
             for (size_t x = 0; x < frame->width; x += 1)
             {
-                uint8_t r = rgb[3 * i], g = rgb[3 * i + 1], b = rgb[3 * i + 2];
+                uint8_t r = rgb[i], g = rgb[i + 1], b = rgb[i + 2];
+                rgb += 3;
+
+                frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
+            }
+        }
+    }
+}
+
+static void RGB_2_I420(const Tensor& t, obs_source_frame* frame)
+{
+    uint32_t image_size = frame->width * frame->height;
+    uint32_t upos = 0;
+    uint32_t vpos = 0;
+    uint32_t i = 0;
+
+    for (uint32_t y = 0; y < frame->height; ++y)
+    {
+        if (!(y & 1))
+        {
+            for (uint32_t x = 0; x < frame->width; x += 2)
+            {
+                uint8_t r = (uint8_t)t(x, y, 0), g = (uint8_t)t(x, y, 1), b = (uint8_t)t(x, y, 2);
+                
+                frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
+                frame->data[1][upos++] = ((-38 * r + -74 * g + 112 * b) >> 8) + 128;
+                frame->data[2][vpos++] = ((112 * r + -94 * g + -18 * b) >> 8) + 128;
+
+                r = (uint8_t)t(x, y, 0);
+                g = (uint8_t)t(x, y, 1);
+                b = (uint8_t)t(x, y, 2);
+
+                frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
+            }
+        }
+        else
+        {
+            for (uint32_t x = 0; x < frame->width; x += 1)
+            {
+                uint8_t r = (uint8_t)t(x, y, 0), g = (uint8_t)t(x, y, 1), b = (uint8_t)t(x, y, 2);
+
                 frame->data[0][i++] = ((66 * r + 129 * g + 25 * b) >> 8) + 16;
             }
         }
@@ -86,6 +196,33 @@ static void I420_2_RGB(const obs_source_frame* frame, uint8_t* rgb)
             rgb[1] = clamp(SCALEYUV(gcoeff(y, u, v)));
             rgb[2] = clamp(SCALEYUV(bcoeff(y, u, v)));
             rgb += 3;
+        }
+
+        py += frame->linesize[0];
+        if (j & 1)
+        {
+            pu += frame->linesize[1];
+            pv += frame->linesize[2];
+        }
+    }
+}
+
+static void I420_2_RGB(const obs_source_frame* frame, Tensor& t)
+{
+    uint8_t* py = frame->data[0];
+    uint8_t* pu = frame->data[1];
+    uint8_t* pv = frame->data[2];
+
+    for (uint32_t j = 0; j < frame->height; ++j)
+    {
+        for (uint32_t i = 0; i < frame->width; ++i)
+        {
+            int y = py[i] - 16;
+            int u = pu[i / 2] - 128;
+            int v = pv[i / 2] - 128;
+            t(i, j, 0) = (float)clamp(SCALEYUV(rcoeff(y, u, v)));
+            t(i, j, 1) = (float)clamp(SCALEYUV(gcoeff(y, u, v)));
+            t(i, j, 2) = (float)clamp(SCALEYUV(bcoeff(y, u, v)));
         }
 
         py += frame->linesize[0];
@@ -170,8 +307,7 @@ static void style_filter_update(void* data, obs_data_t* settings)
     filter->audio_delay_reached = false;
 }
 
-static void* style_filter_create(obs_data_t* settings,
-    obs_source_t* context)
+static void* style_filter_create(obs_data_t* settings, obs_source_t* context)
 {
     style_data* filter = (style_data*)bzalloc(sizeof(style_data));
     obs_audio_info oai;
@@ -181,6 +317,8 @@ static void* style_filter_create(obs_data_t* settings,
 
     obs_get_audio_info(&oai);
     filter->samplerate = oai.samples_per_sec;
+
+    Tensor::SetForcedOpMode(GPU);
 
     return filter;
 }
@@ -244,30 +382,30 @@ static obs_source_frame* style_filter_video(void* data, obs_source_frame* frame)
 
     if (capture_frame)
     {
+        if (!generator)
+        {
+            input = new Placeholder(Shape(frame->width, frame->height, 3), "input");
+            generator = create_generator_model(VGG16::Preprocess(input, NCHW));
+            stylizedContentPre = generator->Outputs()[0];
+            generator->LoadWeights("e:/mosaic_weights.h5", false, true);
+        }
+
         if (frame->format == VIDEO_FORMAT_I420)
         {
-            vector<uint8_t> rgb_data;
-            rgb_data.resize(frame->width * frame->height * 3);
+            /*vector<uint8_t> rgb_data;
+            rgb_data.resize(frame->width * frame->height * 3);*/
+            Tensor frameData(Shape(frame->width, frame->height, 3));
+            frameData.OverrideHost();
 
-            I420_2_RGB(frame, &rgb_data[0]);
+            I420_2_RGB(frame, frameData);
 
-            //Tensor::SetForcedOpMode(GPU);
-            //Tensor testContent(Shape(320, 320, 3));
-            //SampleImagesBatch(LoadFilesList("e:/Downloads/test_content", false), testContent, true);
-            //testContent.SaveAsImage("_test_content.png", false);
+            auto results = Session::Default()->Run({ stylizedContentPre }, { { input, &frameData } });
+            auto frameDataStylized = *results[0];
+            VGG16::DeprocessImage(frameDataStylized, NCHW);
 
-            ///*const string TEST_FILE = "data/contents/content.jpg";
-            //Tensor testImage = LoadImage(TEST_FILE);*/
-            //auto input = new Placeholder(testContent.GetShape(), "input");
-            //auto inputPre = VGG16::Preprocess(input, NCHW);
-            //auto generator = CreateGeneratorModel(testContent.GetShape().Width(), testContent.GetShape().Height(), new Constant(0));
-            //generator->LoadWeights(string("data/") + STYLE + "_weights.h5", false, true);
-            //auto stylizedContentPre = (*generator)(inputPre, new Constant(0))[0];
+            frameDataStylized.SaveAsImage("e:/_frame.jpg", false);
 
-            //auto results = Session::Default()->Run({ stylizedContentPre }, { { input, &testContent } });
-            //auto genImage = *results[0];
-            //VGG16::DeprocessImage(genImage, NCHW);
-
+            RGB_2_I420(frameDataStylized, frame);
 
             /*Tensor frameT = LoadImage(&rgb_data[0], frame->width, frame->height, EPixelFormat::RGB);
             frameT.SaveAsImage("e:/_frame.jpg", false);
@@ -276,10 +414,10 @@ static obs_source_frame* style_filter_video(void* data, obs_source_frame* frame)
 
             frameT.Add(noise);*/
 
-            for (size_t i = 0; i < rgb_data.size(); ++i)
+            /*for (size_t i = 0; i < rgb_data.size(); ++i)
                 rgb_data[i] = clamp(rgb_data[i] + GlobalRng().Next(-50, 50));
 
-            RGB_2_I420(&rgb_data[0], frame);
+            RGB_2_I420(&rgb_data[0], frame);*/
         }        
     }
 
@@ -381,67 +519,4 @@ bool obs_module_load(void)
 
 void obs_module_unload(void)
 {
-}
-
-class OutputScale : public LayerBase
-{
-public:
-    OutputScale(const string& name = "") : LayerBase(__FUNCTION__, Shape(), name) {}
-protected:
-    virtual vector<TensorLike*> InternalCall(const vector<TensorLike*>& inputNodes, TensorLike* training) override { return { multiply(inputNodes[0], 150.f) }; }
-};
-
-
-static ModelBase* CreateGeneratorModel(uint32_t width, uint32_t height, TensorLike* training)
-{
-    NameScope scope("generator");
-
-    auto residual_block = [&](TensorLike* x, int num)
-    {
-        auto shortcut = x;
-        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_1"))->Call(x)[0];
-        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_1"))->Call(x, training)[0];
-        x = (new Activation(new ReLU(), "resi_relu_" + to_string(num) + "_1"))->Call(x)[0];
-        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_2"))->Call(x)[0];
-        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_2"))->Call(x, training)[0];
-        auto m = (new Merge(SumMerge, nullptr, "resi_add_" + to_string(num)))->Call({ x, shortcut })[0];
-        return m;
-    };
-
-    auto input_o = new Input(Shape(width, height, 3), "input_o");
-
-    auto c1 = (new Conv2D(32, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_1"))->Call(input_o->Outputs())[0];
-    c1 = (new InstanceNormalization("normal_1"))->Call(c1, training)[0];
-    c1 = (new Activation(new ReLU(), "relu_1"))->Call(c1)[0];
-
-    auto c2 = (new Conv2D(64, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_2"))->Call(c1)[0];
-    c2 = (new InstanceNormalization("normal_2"))->Call(c2, training)[0];
-    c2 = (new Activation(new ReLU(), "relu_2"))->Call(c2)[0];
-
-    auto c3 = (new Conv2D(128, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_3"))->Call(c2)[0];
-    c3 = (new InstanceNormalization("normal_3"))->Call(c3, training)[0];
-    c3 = (new Activation(new ReLU(), "relu_3"))->Call(c3)[0];
-
-    auto r1 = residual_block(c3, 1);
-    auto r2 = residual_block(r1, 2);
-    auto r3 = residual_block(r2, 3);
-    auto r4 = residual_block(r3, 4);
-    auto r5 = residual_block(r4, 5);
-
-    auto d1 = (new UpSampling2D(2, "up_1"))->Call(r5)[0];
-    d1 = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_4"))->Call(d1)[0];
-    d1 = (new InstanceNormalization("normal_4"))->Call(d1, training)[0];
-    d1 = (new Activation(new ReLU(), "relu_4"))->Call(d1)[0];
-
-    auto d2 = (new UpSampling2D(2, "up_2"))->Call(d1)[0];
-    d2 = (new Conv2D(32, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_5"))->Call(d2)[0];
-    d2 = (new InstanceNormalization("normal_5"))->Call(d2, training)[0];
-    d2 = (new Activation(new ReLU(), "relu_5"))->Call(d2)[0];
-
-    auto c4 = (new Conv2D(3, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_6"))->Call(d2)[0];
-    c4 = (new InstanceNormalization("normal_6"))->Call(c4, training)[0];
-    c4 = (new Activation(new Tanh(), "tanh_1"))->Call(c4)[0];
-    c4 = (new OutputScale("output"))->Call(c4)[0];
-
-    return new Flow(input_o->Outputs(), { c4 }, "generator_model");
 }
