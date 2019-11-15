3b89b838d9557370a54500a465a52bb5001f2cbb
Cr33zz <pawel.creez.kubiak@gmail.com>
2019-11-14 22:30:14 -0500

    Adding support for adaptive neural style transfer.

--- src/obs-stylefilter.cpp
+++ src/obs-stylefilter.cpp
@@ -5,76 +5,139 @@
 #define SETTING_ENABLED_NAME "enabled"
 #define SETTING_ALPHA_TEXT "Style ratio"
 #define SETTING_ALPHA_NAME "alpha"
+#define SETTING_STYLE_FILE_NAME "Style"
+#define SETTING_STYLE_FILE_TEXT "style"
 
 using namespace Neuro;
 
 static ModelBase* generator = nullptr;
-static Placeholder* input = nullptr;
-static TensorLike* stylizedContentPre = nullptr;
+static Placeholder* input_content = nullptr;
+static Placeholder* input_style = nullptr;
+static Placeholder* input_alpha = nullptr;
+static TensorLike* stylized = nullptr;
 
-class OutputScale : public LayerBase
+class AdaIN : public LayerBase
 {
 public:
-    OutputScale(const string& name = "") : LayerBase(__FUNCTION__, Shape(), name) {}
+    AdaIN(TensorLike* alpha, const string& name = "") : LayerBase(__FUNCTION__, Shape(), name), m_Alpha(alpha) {}
 protected:
-    virtual vector<TensorLike*> InternalCall(const vector<TensorLike*>& inputNodes, TensorLike* training) override { return { multiply(inputNodes[0], 150.f) }; }
+    virtual vector<TensorLike*> InternalCall(const vector<TensorLike*>& inputNodes, TensorLike* training) override
+    {
+        auto contentFeat = inputNodes[0];
+        auto styleFeat = inputNodes[1];
+
+        auto styleMean = mean(styleFeat, _01Axes);
+        auto styleStd = std_deviation(styleFeat, styleMean, _01Axes);
+
+        auto normContentFeat = instance_norm(contentFeat, styleStd, styleMean, 0.00001f, training);
+        return { add(multiply(normContentFeat, m_Alpha), multiply(contentFeat, add(negative(m_Alpha), 1))) };
+    }
+
+    TensorLike* m_Alpha;
 };
 
-static ModelBase* create_generator_model(TensorLike* input)
+static ModelBase* create_generator_model(TensorLike* contentPre, TensorLike* stylePre, TensorLike* alpha, Flow& vggEncoder)
 {
     NameScope scope("generator");
 
     auto training = new Constant(0);
 
-    auto residual_block = [&](TensorLike* x, int num)
-    {
-        auto shortcut = x;
-        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_1"))->Call(x)[0];
-        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_1"))->Call(x, training)[0];
-        x = (new Activation(new ReLU(), "resi_relu_" + to_string(num) + "_1"))->Call(x)[0];
-        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_2"))->Call(x)[0];
-        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_2"))->Call(x, training)[0];
-        auto m = (new Merge(SumMerge, nullptr, "resi_add_" + to_string(num)))->Call({ x, shortcut })[0];
-        return m;
-    };
+    auto inputContent = new Input(contentPre, "input_content");
+    auto inputStyle = new Input(stylePre, "input_style");
 
-    auto input_o = new Input(input, "input_o");
+    // encoder
 
-    auto c1 = (new Conv2D(32, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_1"))->Call(input_o->Outputs())[0];
-    c1 = (new InstanceNormalization("normal_1"))->Call(c1, training)[0];
-    c1 = (new Activation(new ReLU(), "relu_1"))->Call(c1)[0];
+    auto contentFeat = vggEncoder(inputContent->Outputs(), nullptr, "content_features").back();
+    auto styleFeat = vggEncoder(inputStyle->Outputs(), nullptr, "style_features").back();
 
-    auto c2 = (new Conv2D(64, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_2"))->Call(c1)[0];
-    c2 = (new InstanceNormalization("normal_2"))->Call(c2, training)[0];
-    c2 = (new Activation(new ReLU(), "relu_2"))->Call(c2)[0];
+    // adaptive instance normalization
 
-    auto c3 = (new Conv2D(128, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_3"))->Call(c2)[0];
-    c3 = (new InstanceNormalization("normal_3"))->Call(c3, training)[0];
-    c3 = (new Activation(new ReLU(), "relu_3"))->Call(c3)[0];
+    auto adaInFeat = (new AdaIN(alpha))->Call({ contentFeat, styleFeat }, training, "ada_in")[0];
 
-    auto r1 = residual_block(c3, 1);
-    auto r2 = residual_block(r1, 2);
-    auto r3 = residual_block(r2, 3);
-    auto r4 = residual_block(r3, 4);
-    auto r5 = residual_block(r4, 5);
+    // decoder
 
-    auto d1 = (new UpSampling2D(2, "up_1"))->Call(r5)[0];
-    d1 = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_4"))->Call(d1)[0];
-    d1 = (new InstanceNormalization("normal_4"))->Call(d1, training)[0];
-    d1 = (new Activation(new ReLU(), "relu_4"))->Call(d1)[0];
+    auto d = (new Conv2D(256, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block4_conv1"))->Call(adaInFeat)[0];
+    d = (new UpSampling2D(2, "decode_up_1"))->Call(d)[0];
 
-    auto d2 = (new UpSampling2D(2, "up_2"))->Call(d1)[0];
-    d2 = (new Conv2D(32, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_5"))->Call(d2)[0];
-    d2 = (new InstanceNormalization("normal_5"))->Call(d2, training)[0];
-    d2 = (new Activation(new ReLU(), "relu_5"))->Call(d2)[0];
+    d = (new Conv2D(256, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block3_conv4"))->Call(d)[0];
+    d = (new Conv2D(256, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block3_conv3"))->Call(d)[0];
+    d = (new Conv2D(256, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block3_conv2"))->Call(d)[0];
+    d = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block3_conv1"))->Call(d)[0];
+    d = (new UpSampling2D(2, "decode_up_2"))->Call(d)[0];
 
-    auto c4 = (new Conv2D(3, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_6"))->Call(d2)[0];
-    c4 = (new InstanceNormalization("normal_6"))->Call(c4, training)[0];
-    c4 = (new Activation(new Tanh(), "tanh_1"))->Call(c4)[0];
-    c4 = (new OutputScale("output"))->Call(c4)[0];
+    d = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block2_conv2"))->Call(d)[0];
+    d = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block2_conv1"))->Call(d)[0];
+    d = (new UpSampling2D(2, "decode_up_3"))->Call(d)[0];
 
-    return new Flow(input_o->Outputs(), { c4 }, "generator_model");
+    d = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), new ReLU(), NCHW, "decode_block1_conv2"))->Call(d)[0];
+    d = (new Conv2D(3, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "decode_block1_conv1"))->Call(d)[0];
+
+    return new Flow({ inputContent->Outputs()[0], inputStyle->Outputs()[0] }, { d, adaInFeat }, "generator_model");
 }
+
+//class OutputScale : public LayerBase
+//{
+//public:
+//    OutputScale(const string& name = "") : LayerBase(__FUNCTION__, Shape(), name) {}
+//protected:
+//    virtual vector<TensorLike*> InternalCall(const vector<TensorLike*>& inputNodes, TensorLike* training) override { return { multiply(inputNodes[0], 150.f) }; }
+//};
+//
+//static ModelBase* create_generator_model(TensorLike* input)
+//{
+//    NameScope scope("generator");
+//
+//    auto training = new Constant(0);
+//
+//    auto residual_block = [&](TensorLike* x, int num)
+//    {
+//        auto shortcut = x;
+//        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_1"))->Call(x)[0];
+//        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_1"))->Call(x, training)[0];
+//        x = (new Activation(new ReLU(), "resi_relu_" + to_string(num) + "_1"))->Call(x)[0];
+//        x = (new Conv2D(128, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "resi_conv_" + to_string(num) + "_2"))->Call(x)[0];
+//        x = (new InstanceNormalization("resi_normal_" + to_string(num) + "_2"))->Call(x, training)[0];
+//        auto m = (new Merge(SumMerge, nullptr, "resi_add_" + to_string(num)))->Call({ x, shortcut })[0];
+//        return m;
+//    };
+//
+//    auto input_o = new Input(input, "input_o");
+//
+//    auto c1 = (new Conv2D(32, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_1"))->Call(input_o->Outputs())[0];
+//    c1 = (new InstanceNormalization("normal_1"))->Call(c1, training)[0];
+//    c1 = (new Activation(new ReLU(), "relu_1"))->Call(c1)[0];
+//
+//    auto c2 = (new Conv2D(64, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_2"))->Call(c1)[0];
+//    c2 = (new InstanceNormalization("normal_2"))->Call(c2, training)[0];
+//    c2 = (new Activation(new ReLU(), "relu_2"))->Call(c2)[0];
+//
+//    auto c3 = (new Conv2D(128, 3, 2, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_3"))->Call(c2)[0];
+//    c3 = (new InstanceNormalization("normal_3"))->Call(c3, training)[0];
+//    c3 = (new Activation(new ReLU(), "relu_3"))->Call(c3)[0];
+//
+//    auto r1 = residual_block(c3, 1);
+//    auto r2 = residual_block(r1, 2);
+//    auto r3 = residual_block(r2, 3);
+//    auto r4 = residual_block(r3, 4);
+//    auto r5 = residual_block(r4, 5);
+//
+//    auto d1 = (new UpSampling2D(2, "up_1"))->Call(r5)[0];
+//    d1 = (new Conv2D(64, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_4"))->Call(d1)[0];
+//    d1 = (new InstanceNormalization("normal_4"))->Call(d1, training)[0];
+//    d1 = (new Activation(new ReLU(), "relu_4"))->Call(d1)[0];
+//
+//    auto d2 = (new UpSampling2D(2, "up_2"))->Call(d1)[0];
+//    d2 = (new Conv2D(32, 3, 1, Tensor::GetPadding(Same, 3), nullptr, NCHW, "conv_5"))->Call(d2)[0];
+//    d2 = (new InstanceNormalization("normal_5"))->Call(d2, training)[0];
+//    d2 = (new Activation(new ReLU(), "relu_5"))->Call(d2)[0];
+//
+//    auto c4 = (new Conv2D(3, 9, 1, Tensor::GetPadding(Same, 9), nullptr, NCHW, "conv_6"))->Call(d2)[0];
+//    c4 = (new InstanceNormalization("normal_6"))->Call(c4, training)[0];
+//    c4 = (new Activation(new Tanh(), "tanh_1"))->Call(c4)[0];
+//    c4 = (new OutputScale("output"))->Call(c4)[0];
+//
+//    return new Flow(input_o->Outputs(), { c4 }, "generator_model");
+//}
 
 #define SCALEYUV(v) (((v)+128000)/256000)
 
@@ -223,7 +286,8 @@ struct style_data
 {
     obs_source_t* context;
     bool enabled = false;
-    float alpha = 0.f;
+    Tensor alpha = Tensor({ 1.f }, Shape(1));
+    Tensor style;
 };
 
 static const char* style_filter_name(void* unused)
@@ -236,7 +300,8 @@ static void style_filter_update(void* data, obs_data_t* settings)
 {
     style_data* filter = (style_data*)data;
     filter->enabled = (bool)obs_data_get_bool(settings, SETTING_ENABLED_NAME);
-    filter->alpha = (float)obs_data_get_double(settings, SETTING_ALPHA_NAME);
+    filter->alpha(0) = (float)obs_data_get_double(settings, SETTING_ALPHA_NAME);
+    filter->style = LoadImage(obs_data_get_string(settings, SETTING_STYLE_FILE_NAME), 512, 512, 512, 512);
 }
 
 static void* style_filter_create(obs_data_t* settings, obs_source_t* context)
@@ -263,6 +328,8 @@ static obs_properties_t* style_filter_properties(void* data)
     
     obs_properties_add_bool(props, SETTING_ENABLED_NAME, SETTING_ENABLED_TEXT);
     obs_properties_add_float(props, SETTING_ALPHA_NAME, SETTING_ALPHA_TEXT, 0, 1, 0.01);
+    static string default_style = string(obs_get_module_data_path(obs_current_module())) + "/mosaic.jpg";
+    obs_properties_add_path(props, SETTING_STYLE_FILE_NAME, SETTING_STYLE_FILE_TEXT, OBS_PATH_FILE, "*.jpg", default_style.c_str());
     
     UNUSED_PARAMETER(data);
     return props;
@@ -282,12 +349,35 @@ static obs_source_frame* style_filter_video(void* data, obs_source_frame* frame)
     {
         if (!generator)
         {
-            input = new Placeholder(Shape(frame->width, frame->height, 3), "input");
+            input_content = new Placeholder(Shape(frame->width, frame->height, 3), "input_content");
+            input_style = new Placeholder(Shape(frame->width, frame->height, 3), "input_style");
+            input_alpha = new Placeholder(filter->alpha, "alpha");
+
+            auto vggModel = VGG19::CreateModel(NCHW, Shape(frame->width, frame->height, 3), false, MaxPool, "data/");
+            vggModel->SetTrainable(false);
+
+            vector<TensorLike*> styleOutputs = { vggModel->Layer("block1_conv1")->Outputs()[0],
+                                                 vggModel->Layer("block2_conv1")->Outputs()[0],
+                                                 vggModel->Layer("block3_conv1")->Outputs()[0],
+                                                 vggModel->Layer("block4_conv1")->Outputs()[0] };
+
+            auto vggEncoder = Flow(vggModel->InputsAt(-1), styleOutputs, "vgg_features");
+
+            auto contentPre = VGG16::Preprocess(input_content, NCHW, false);
+            auto stylePre = VGG16::Preprocess(input_style, NCHW, false);
+
+            auto generator = create_generator_model(contentPre, stylePre, input_alpha, vggEncoder);
+            string data_path = obs_get_module_data_path(obs_current_module());
+            generator->LoadWeights(data_path + "/adaptive_weights.h5", false, true);
+
+            stylized = generator->Outputs()[0];
+
+            /*input = new Placeholder(Shape(frame->width, frame->height, 3), "input");
             generator = create_generator_model(VGG16::Preprocess(input, NCHW));
-            stylizedContentPre = generator->Outputs()[0];
+            stylized = generator->Outputs()[0];
 
             string data_path = obs_get_module_data_path(obs_current_module());
-            generator->LoadWeights(data_path + "/mosaic_weights.h5", false, true);
+            generator->LoadWeights(data_path + "/mosaic_weights.h5", false, true);*/
         }
 
         Tensor frameData(Shape(frame->width, frame->height, 3));
@@ -300,9 +390,10 @@ static obs_source_frame* style_filter_video(void* data, obs_source_frame* frame)
 
         //frameData.SaveAsImage("e:/_frame.jpg", false);
 
-        auto results = Session::Default()->Run({ stylizedContentPre }, { { input, &frameData } });
+        //auto results = Session::Default()->Run({ stylized }, { { input_content, &frameData } });
+        auto results = Session::Default()->Run({ stylized }, { { input_content, &frameData }, { input_style, &filter->style }, { input_alpha, &filter->alpha } });
         auto frameDataStylized = *results[0];
-        VGG16::DeprocessImage(frameDataStylized, NCHW);
+        //VGG16::DeprocessImage(frameDataStylized, NCHW);
 
         //frameDataStylized.SaveAsImage("e:/_frame_s.jpg", false);
 
